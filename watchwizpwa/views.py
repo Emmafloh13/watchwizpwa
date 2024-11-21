e881f6baefdd27e99b742d95cc5ed750596dff5d
Emmafloh13 <emmfh342@gmail.com>
2024-11-20 21:54:27 -0600

    Subiendo archivos para el header y el menu

--- watchwizpwa/views.py
+++ watchwizpwa/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 
+from watchwiz.services.services_data import obtener_datos_empresa
+
 def home_view(request):
         #Verificar si el usuario esta en la bd
         if not request.session.get('authenticated'):
